0fe2e520b0a59d6172184dce9028eb8540650436
Camilo Dubra Corchs <camilodubra29@gmail.com>
2026-01-17 18:09:24 +0100

    Añadidas imagenes y corregido error de title

--- views/listar.php
+++ views/listar.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Listado de Alumnos (MVC)</title>
+    <title>Listado de Mascotas (MVC)</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="/CRUD-PHP/assets/css/login.css">
 </head>
